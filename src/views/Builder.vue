df9761c2de21568fd771c51fe2b2507d758af1ed
Lars Lemmens <71447000+LarsLemmens@users.noreply.github.com>
2022-04-21 12:32:14 +0100

    fixes css

--- src/views/Builder.vue
+++ src/views/Builder.vue
@@ -100,8 +100,6 @@ export default {
 
 
 <style>
-@import "../assets/css/bootstrap.min.css";
-
 @import "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800";
 @import "https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800";
 @import "https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic";
@@ -114,7 +112,6 @@ export default {
 
 
 <style scoped>
-@import "../assets/css/bootstrap.min.builder.css";
 @import "../assets/css/Projects-Clean.css";
 @import "../assets/css/Features-Boxed.css";
 </style>
